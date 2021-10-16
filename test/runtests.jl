ce9c71eaa29914b9b7e5f7088123ed6d7dd30fce
David Widmann <david.widmann@it.uu.se>
2021-10-16 22:20:56 +0200

    Add `realdot`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,34 @@
 using RealDot
+using LinearAlgebra
 using Test
 
+# struct need to be defined outside of tests for julia 1.0 compat
+# custom complex number to test fallback definition
+struct CustomComplex{T}
+    re::T
+    im::T
+end
+
+Base.real(x::CustomComplex) = x.re
+Base.imag(x::CustomComplex) = x.im
+
+function LinearAlgebra.dot(a::CustomComplex, b::Number)
+    return CustomComplex(reim((a.re - a.im * im) * b)...)
+end
+function LinearAlgebra.dot(a::Number, b::CustomComplex)
+    return CustomComplex(reim(conj(a) * (b.re + b.im * im))...)
+end
+function LinearAlgebra.dot(a::CustomComplex, b::CustomComplex)
+    return CustomComplex(reim((a.re - a.im * im) * (b.re + b.im * im))...)
+end
+
 @testset "RealDot.jl" begin
-    # Write your tests here.
+    scalars = (randn(), randn(ComplexF64), CustomComplex(reim(randn(ComplexF64))...))
+    arrays = (randn(10), randn(ComplexF64, 10))
+
+    for inputs in (scalars, arrays)
+        for x in inputs, y in inputs
+            @test realdot(x, y) == real(dot(x, y))
+        end
+    end
 end
