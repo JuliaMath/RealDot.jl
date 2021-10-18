1ecb63d61b301c1551c5da4000d177bd1027f330
David Widmann <david.widmann@it.uu.se>
2021-10-18 16:30:08 +0200

    Add test with quaternions

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using LinearAlgebra
 using Test
 
 # struct need to be defined outside of tests for julia 1.0 compat
-# custom complex number to test fallback definition
-struct CustomComplex{T}
+# custom complex number (tests fallback definition)
+struct CustomComplex{T} <: Number
     re::T
     im::T
 end
@@ -12,18 +12,47 @@ end
 Base.real(x::CustomComplex) = x.re
 Base.imag(x::CustomComplex) = x.im
 
-function LinearAlgebra.dot(a::CustomComplex, b::Number)
-    return CustomComplex(reim((a.re - a.im * im) * b)...)
+Base.conj(x::CustomComplex) = CustomComplex(x.re, -x.im)
+
+function Base.:*(x::CustomComplex, y::Union{Real,Complex})
+    return CustomComplex(reim(Complex(reim(x)...) * y)...)
 end
-function LinearAlgebra.dot(a::Number, b::CustomComplex)
-    return CustomComplex(reim(conj(a) * (b.re + b.im * im))...)
-end
-function LinearAlgebra.dot(a::CustomComplex, b::CustomComplex)
-    return CustomComplex(reim((a.re - a.im * im) * (b.re + b.im * im))...)
+Base.:*(x::Union{Real,Complex}, y::CustomComplex) = y * x
+function Base.:*(x::CustomComplex, y::CustomComplex)
+    return CustomComplex(reim(Complex(reim(x)...) * Complex(reim(y)...))...)
 end
 
+# custom quaternion to test definition for hypercomplex numbers
+# adapted from Quaternions.jl
+struct Quaternion{T<:Real} <: Number
+    s::T
+    v1::T
+    v2::T
+    v3::T
+end
+
+Base.real(q::Quaternion) = q.s
+Base.conj(q::Quaternion) = Quaternion(q.s, -q.v1, -q.v2, -q.v3)
+
+function Base.:*(q::Quaternion, w::Quaternion)
+    return Quaternion(
+        q.s * w.s - q.v1 * w.v1 - q.v2 * w.v2 - q.v3 * w.v3,
+        q.s * w.v1 + q.v1 * w.s + q.v2 * w.v3 - q.v3 * w.v2,
+        q.s * w.v2 - q.v1 * w.v3 + q.v2 * w.s + q.v3 * w.v1,
+        q.s * w.v3 + q.v1 * w.v2 - q.v2 * w.v1 + q.v3 * w.s,
+    )
+end
+
+function Base.:*(q::Quaternion, w::Union{Real,Complex,CustomComplex})
+    a, b = reim(w)
+    return q * Quaternion(a, b, zero(a), zero(a))
+end
+Base.:*(q::Union{Real,Complex,CustomComplex}, w::Quaternion) = w * q
+
 @testset "RealDot.jl" begin
-    scalars = (randn(), randn(ComplexF64), CustomComplex(reim(randn(ComplexF64))...))
+    scalars = (
+        randn(), randn(ComplexF64), CustomComplex(randn(2)...), Quaternion(randn(4)...)
+    )
     arrays = (randn(10), randn(ComplexF64, 10))
 
     for inputs in (scalars, arrays)
